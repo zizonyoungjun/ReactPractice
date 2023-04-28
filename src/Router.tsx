7e24b7a8db1f975bd5a10f61045ab3f43077c88b
zizonyoungjun <naya700@naver.com>
2023-04-28 12:17:46 +0900

    feat: BrowserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/:coinId' element={<Coins />}></Route>
+                <Route path='/:coinId' element={<Coin />}></Route>
             </Routes>
             <Routes>
                 <Route path='/' element={<Coins />}></Route>
