a424d2be8b0fe195e6a4499bb584d9a185562db1
zizonyoungjun <naya700@naver.com>
2023-05-12 12:45:25 +0900

    fix: DefaultTheme

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,6 @@ import { DefaultTheme } from 'styled-components';
 
 export const theme :DefaultTheme = {
     textColor:"white",
-    bgColor:"white",
+    bgColor:"black",
     btnColor:"tomato",
 }
