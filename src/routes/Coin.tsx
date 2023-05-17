cb9cec86cc01f9e7eda535da049b8396811dd7e4
zizonyoungjun <naya700@naver.com>
2023-05-17 16:00:17 +0900

    feat: useParams

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useParams} from "react-router";
+
+interface RouteParams {
+    coinId: string;
+}
 
 function Coin() {
+    const{ coinId } = useParams();
     return (
         <div>
-            <h1>detail</h1>
+            <h1>Coin: {coinId}</h1>
         </div>
     );
 }
